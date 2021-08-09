a835c5a6f62bca3ffe63126449ddd4f0fa624b91
Andrei Bastos <andreibastos@outlook.com>
2021-08-08 21:25:28 -0300

    adicionado comentários

--- internet/internet.ino
+++ internet/internet.ino
@@ -5,36 +5,50 @@
  @date: 08/08/2021
 */
 
-#include <WiFi.h>
-#include <HTTPClient.h>
-#include <ArduinoJson.h>
+/* 
+ * bibliotecas necessárias 
+ */
+#include <WiFi.h> // wifi
+#include <HTTPClient.h> // requisições http
+#include <ArduinoJson.h> // json 
 
+/*
+ * identificação da rede wifi
+ */
 char* ssid = "SSID";
 char* password = "PASSWORD";
 
-const char* endpointAPI = "https://agendamento.vitoria.es.gov.br/api/categorias/2/servicos";
+/* 
+ *  variáveis globais
+ */  
+const char* endpointAPI = "https://agendamento.vitoria.es.gov.br/api/categorias/2/servicos"; // url da api da prefeitura
 uint16_t CAPACITY = 6144; // alocação de memoria dinâmica
 uint16_t period = 60000; // tempo entre requisições // 60 segundos
 
+// configuração inicial
 void setup() {
-  Serial.begin(115200);   
-  connectWifi();  
+  Serial.begin(115200);
+  connectWifi();   // conectar na wifi
 }
 
+// loop principal
 void loop() {
-  if(WiFi.status()== WL_CONNECTED){
-        JsonArray services = getServices();
-        printServices(services);
+  // se tiver conectado na wifi
+  if(WiFi.status()== WL_CONNECTED){        
+        JsonArray services = getServices(); // obtem a lista de serviços        
+        printServices(services); // imprime os serviços
   }
-  delay(period);
+  delay(period); // aguarda o periodo
 }
 
+// conectar na wifi
 void connectWifi() {
   delay(10);
   Serial.println();
   Serial.print("Tentando se conectar a : ");
   Serial.println(ssid);
 
+  // tenta conectar com as credenciais
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -48,67 +62,64 @@ void connectWifi() {
 }
 
 
+// função que retorna a lista de serviços como jsonArray
 JsonArray getServices() {
   Serial.print("obtendo serviços de ");
   Serial.println(endpointAPI);
-  String servicesResponse = httpGETRequest(endpointAPI);
-  JsonArray services =  stringToDocument(servicesResponse);  
-  return services; 
+  String servicesResponse = httpGETRequest(endpointAPI); // requisição de get 
+  JsonArray services =  stringToDocument(servicesResponse); // transforma para array de elementos
+  return services; // retorna a lista de serviços
 }
 
+// função que imprime os serviços
 void printServices(JsonArray services){      
-  for(JsonObject service : services) {      
-     
-  int id = service["id"]; 
-  const char* nome = service["nome"]; 
-  const char* descricao = service["descricao"]; 
+  for(JsonObject service : services) {           
+    int id = service["id"]; 
+    const char* nome = service["nome"]; 
+    const char* descricao = service["descricao"]; 
+  
+    Serial.print("id: ");
+    Serial.println(id);
+  
+    Serial.print("nome: ");
+    Serial.println(nome);  
 
-  Serial.print("id: ");
-  Serial.println(id);
-
-  Serial.print("nome: ");
-  Serial.println(nome);
-
-  /*
-  Serial.print("descrição: ");
-  Serial.println(descricao);
-  */
-
-  Serial.println("----------------");
-  Serial.println(" ");
+    Serial.println("----------------");
+    Serial.println(" ");
   }
 }
 
+// função que realiza um get em um endpoint qualquer
 String httpGETRequest(const char* endpoint) {
   HTTPClient http;
   String payload = "{}"; 
-    
-  http.begin(endpoint);  
-  int httpResponseCode = http.GET();    
-  if (httpResponseCode>0) {    
-    payload = http.getString();
+  
+  http.begin(endpoint); // prepara a requisição
+  int httpResponseCode = http.GET(); // realiza a requisição get
+  if (httpResponseCode==200) {  // se a requisição deu sucesso
+    payload = http.getString(); // recebe o response
   }
   else {
     Serial.print("Error code: ");
     Serial.println(httpResponseCode);    
   }    
-  http.end();
+  http.end(); // finaliza a requisição
   
-  return payload;
+  return payload; // retorna o payload 
 }
 
+// função que converte string para document
 JsonArray stringToDocument(String jsonString){   
-  DynamicJsonDocument doc(CAPACITY);
-  
-  DeserializationError error = deserializeJson(doc, jsonString);
+  DynamicJsonDocument doc(CAPACITY); // capacidade do documento
 
-  // Test if parsing succeeds.
+  // tratativa de erro
+  DeserializationError error = deserializeJson(doc, jsonString);  
   if (error) {
-    Serial.print(F("deserializeJson() failed: "));
+    Serial.print(F("deserializeJson() falhou: "));
     Serial.println(error.f_str());
-    return doc.to<JsonArray>();
+    return doc.to<JsonArray>(); // retorna lisa vazia
   } 
 
-  JsonArray array = doc.as<JsonArray>();
-  return array;
+  JsonArray array = doc.as<JsonArray>(); // transforma para array
+  return array; // retorna o array
 }
